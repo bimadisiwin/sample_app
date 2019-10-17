5fe759a18273a92cd66e0ed4dc98f37d3f2e0b2f
Yusuke Yonekura <bimadisiwin@me.com>
2019-10-17 14:23:36 +0700

    Finish user edit, update, index, and destroy actions

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,9 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                      password_confirmation: "bar"} }
       end
       assert_template 'users/new'
-      assert_select 'div#error_explanation'
-      assert_select 'div.alert'
-      assert_select 'form[action="/signup"]'
   end
 
   test "valid signup information" do
